2c417a41a9bb99af8268c89793b4fb2b0785deaf
Matthew Chandran <chandran.31@buckeyemail.osu.edu>
2025-12-03 11:56:48 -0500

    added intro component

---
+++ app/assets/javascripts/components/intro.js.jsx
@@ -0,0 +1,14 @@
+
+function Intro(props) {
+  return (
+    <React.Fragment>
+      <section className="intro">
+        <h2>Welcome to Split the Bill</h2>
+        <p>Going on a trip with friends? Keep track of expenses, balances, and settlements all in one place.</p>
+      </section>
+    </React.Fragment>
+  );
+}
+
+
+
