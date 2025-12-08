f217e6d7196d8103879de79935dd41d3896dfbea
Aaron <geise.51@buckeyemail.osu.edu>
2025-12-07 19:42:31 -0500

    pre merge commit

--- db/schema.rb
+++ db/schema.rb
@@ -31,15 +31,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_03_064234) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "trip_participants", force: :cascade do |t|
-    t.integer "trip_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["trip_id"], name: "index_trip_participants_on_trip_id"
-    t.index ["user_id"], name: "index_trip_participants_on_user_id"
-  end
-
   create_table "trips", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
@@ -61,6 +52,4 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_03_064234) do
 
   add_foreign_key "expenses", "trips"
   add_foreign_key "posts", "users"
-  add_foreign_key "trip_participants", "trips"
-  add_foreign_key "trip_participants", "users"
 end
